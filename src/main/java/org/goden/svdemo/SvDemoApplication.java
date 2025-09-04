c6d4558793e772a208c48e2dec5254d07d03d09e
sliar <sliar@gemlynn.com.cn>
2025-09-04 10:00:45 +0800

    第一次上传

---
+++ src/main/java/org/goden/svdemo/SvDemoApplication.java
@@ -0,0 +1,13 @@
+package org.goden.svdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/goden/svdemo/SvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goden.svdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
