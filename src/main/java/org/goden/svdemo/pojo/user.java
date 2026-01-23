282f5b2b9910bfbad2643877513e5379ad9c11ca
sliar <sliar@gemlynn.com.cn>
2026-01-23 10:08:11 +0800

    1

--- src/main/java/org/goden/svdemo/pojo/user.java
+++ src/main/java/org/goden/svdemo/pojo/user.java
@@ -1,10 +1,13 @@
 package org.goden.svdemo.pojo;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDateTime;
 
 @Data
 public class user {
+    @Id
     private Integer id;
     private String username;
     private String password;
